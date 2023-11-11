00221b7469b8af8c27ec37dcbd65e9dfbf534e56
Amit Singh <amitkarun4559@gmail.com>
2023-11-12 00:02:23 +0530

    First react commit

---
+++ Testimonial App/src/data.js
@@ -0,0 +1,46 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Amit Singh",
+    job: "Full Stack Developer",
+    image: "https://imgur.com/H9Z8kDt.jpg",
+    text: "I have had the pleasure of working with this full-stack developer on several projects, and I am consistently impressed with their technical expertise, problem solving skills, and ability to deliver high-quality solutions on time and within budget. They have a deep understanding of both front-end and back-end development, and They are able to quickly and efficiently develop and deploy complex web applications.",
+  },
+  {
+    id: 2,
+    name: "Nikhil Sharma",
+    job: "Data Analyst",
+    image: "https://imgur.com/5sHPj4h.jpg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 3,
+    name: "Parth Singh",
+    job: "Web3 Developer",
+    image: "https://imgur.com/ryJrWrJ.jpg",
+    text: "I have had the pleasure of working with this Web3 developer on several projects, and I am consistently impressed with their technical expertise, problem-solving skills, and ability to deliver high-quality solutions on time and within budget. They have a deep understanding of blockchain technology, smart contracts, and decentralized applications (dApps).",
+  },
+  {
+    id: 4,
+    name: "Rohit Yadav",
+    job: "Front-end Developer",
+    image: "https://imgur.com/Cn0UUb6.jpg",
+    text: "I have had the pleasure of working with this front-end developer on several projects, and I am consistently impressed with their technical expertise, creativity, and ability to deliver high-quality user experiences on time and within budget. They have a deep understanding of HTML, CSS, and JavaScript, as well as modern front-end frameworks and libraries.",
+  },
+  {
+    id: 5,
+    name: "Sandeep Yadav",
+    job: "Data Scientist",
+    image: "https://imgur.com/VFH9SGK.jpg",
+    text: "I have had the pleasure of working with this data scientist on several projects, and I am consistently impressed with their technical expertise, analytical skills, and ability to communicate complex findings in a clear and concise manner. They have a deep understanding of statistical methods, machine learning algorithms, and data mining techniques.",
+  },
+  {
+    id: 6,
+    name: "Vishal Yadav",
+    job: "SOFTWARE ENGINEER",
+    image: "https://imgur.com/nIvhXWf.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+];
+
+export default reviews;
---
+++ formapp/src/App.js
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // function changeFirstNameHandler(event) {
+  //   // console.log("Printing first name");
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+
+  // function changeLastNameHandler(event) {
+  //   // console.log("Printing last name");
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+
+  const [formData, setFormData] = useState( {firstName:"", lastName:"", email:"", comments:"", isVisible:true, mode:"", favCar:""} );
+
+  // console.log(formData);
+
+  function changeHandler (event) {
+    const {name, value, checked, type} = event.target;
+
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value 
+      }
+    });
+  }
+
+  function submitHandler (event) {
+    event.preventDefault();
+    console.log("Finally printing the entire form ka data .......");
+    console.log(formData);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={submitHandler}>
+        <input type='text' placeholder='First Name' onChange={changeHandler} name='firstName' value={formData.firstName}/>
+
+        <br></br>
+        <br></br>
+        <input type='text' placeholder='Last Name' onChange={changeHandler} 
+        name='lastName' value={formData.lastName}/>
+
+        <br></br>
+        <br></br>
+        <input type='email' placeholder='Enter your email' onChange={changeHandler} 
+        name='email' value={formData.email}/>
+
+        <br></br>
+        <br></br>
+        <textarea placeholder='Enter yours comments' onChange={changeHandler} name='comments' value={formData.comments}/>
+
+        <br></br>
+        <br></br>
+        <input type='checkbox' onChange={changeHandler} 
+        name='isVisible' id="isVisible" checked={formData.isVisible} />
+        <label htmlFor='isVisible'>I am a visible person</label>
+
+        <br></br>
+        <br></br>
+        <input type='radio' onChange={changeHandler} 
+        name='mode' id="Online-mode" value='Online-mode' checked={formData.mode === "Online-mode"} />
+        <label htmlFor='Online-mode'>Online Mode</label>
+
+        <br></br>
+        <br></br>
+        <input type='radio' onChange={changeHandler} 
+        name='mode' id="Offline-mode" value='Offline-mode' checked={formData.mode === "offline-mode"} />
+        <label htmlFor='Offline-mode'>Offline Mode</label>
+
+        <br></br>
+        <br></br>
+        <label htmlFor='favCar'>Tell me your Favourite Car</label>
+        <select name='favCar' onChange={changeHandler} id='favCar' value={formData.favCar}>
+          <option value="Scarpio">Scarpio</option>
+          <option value="Fartuner">Fartuner</option>
+          <option value="Tharrr">Tharrr</option>
+          <option value="Legender">Legender</option>
+          <option value="Defender">Defender</option>
+        </select>
+
+        <br></br>
+        <br></br>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
