7c679ec44b9b66cbbf7884ac969bfdd858f8bdeb
Amit Singh <amitkarun4559@gmail.com>
2023-11-11 23:51:56 +0530

    First react commit

---
+++ top-course-starter/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import Spinner from "./components/Spinner";
+import { apiUrl, filterData } from "./data";
+import { useState} from "react";
+import { toast } from "react-toastify";
+
+const App = () => {
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoding] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title)
+
+  async function fetchData () {
+    setLoding(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      // output -->
+      setCourses(output.data);
+    }catch(error){
+      toast.error("Network mai koi dikkat hai");
+    }
+    setLoding(false);
+  }
+
+  useEffect( () =>{
+    fetchData();
+  }, [])
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        <div>
+          <Filter filterData = {filterData}
+          category = {category}
+          setCategory = {setCategory}/>
+        </div>
+        <div className="w-11/12 max-w-[1200px] mx-auto flex  flex-wrap justify-center items-center min-h-[50vh]">
+          {
+            loading ? (<Spinner/>) : (<Cards courses = {courses} category = {category}/>)
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
